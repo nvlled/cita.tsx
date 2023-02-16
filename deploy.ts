4f93836592dc5c264f59c771953227f1cc08d9c7
Ronald Casili <nvlled@gmail.com>
2023-02-16 19:21:06 +0800

    Add deploy tool

---
+++ deploy.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S deno run -A
+
+// This script does the following:
+// - rebuild all files
+// - regenerate readme
+// - copy to docs (for gh-pages)
+
+import $ from "https://deno.land/x/dax@0.27.0/mod.ts";
+
+await $`./cita.tsx clean`;
+await $`./cita.tsx build`;
+
+const readmeContent = await $`./cita.tsx doc`.text();
+await Deno.writeTextFile("README.md", readmeContent);
+
+const docs = $.path("./docs");
+if (await docs.exists()) {
+  await docs.remove({ recursive: true });
+}
+
+await docs.mkdir({ recursive: true });
+await $.fs.copy("./_build/", "./docs", { overwrite: true });
